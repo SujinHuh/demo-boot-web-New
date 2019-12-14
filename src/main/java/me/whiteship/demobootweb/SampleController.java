c5400107c0aede279c6e9ccf0b7838d012213cfa
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-15 00:08:56 +0900

    Handler Interceptor
    /*
        perHandle 1
        perHandle 2
        요청 처리
        postHandle 2
        postHandle 1
        뷰 랜더링
        afterCompletion 2
        afterCompletion 1
         */

--- src/main/java/me/whiteship/demobootweb/SampleController.java
+++ src/main/java/me/whiteship/demobootweb/SampleController.java
@@ -9,6 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    /*
+    perHandle 1
+    perHandle 2
+    요청 처리
+    postHandle 2
+    postHandle 1
+    뷰 랜더링
+    afterCompletion 2
+    afterCompletion 1
+     */
+
     @GetMapping("/hello")
     public String hello(@RequestParam("id") Person person) {
      return "hello " + person.getName();
--- src/main/java/me/whiteship/demobootweb/WebConfig.java
+++ src/main/java/me/whiteship/demobootweb/WebConfig.java
@@ -2,9 +2,16 @@ package me.whiteship.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new GreetingInterceptor());
+        registry.addInterceptor(new AnotherInterceptor());
+    }
 }
