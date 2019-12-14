6e796d3d2bcffed67687691f3585428c12db9cb6
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-14 23:10:10 +0900

    Domain Class Converter Auto Registration (도메인 클래스 컨버터 자동 등록)   - 테스트용 이벤트 객체 생성   - 이벤트 리파지토리에 저장   - 저장한 이벤트의 ID로 조회 시도

--- src/main/java/me/whiteship/demobootweb/Person.java
+++ src/main/java/me/whiteship/demobootweb/Person.java
@@ -1,6 +1,15 @@
 package me.whiteship.demobootweb;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+
 public class Person {
+
+    @Id @GeneratedValue
+    private Long id;
 
     private String name;
 
@@ -12,5 +21,11 @@ public class Person {
         this.name = name;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
